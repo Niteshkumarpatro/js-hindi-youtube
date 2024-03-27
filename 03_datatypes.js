bb1cb4136692998531488546a678dead0707cc23
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-03-27 13:44:47 +0530

    Datatypes

---
+++ 03_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+//alert("3+3") // we are using Nodejs,not browser
+
+console.log(3+3) // code readability should be high
+
+console.log("nitesh")
+
+let name = "nitesh"
+let age =18
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// strung => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol =>unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
