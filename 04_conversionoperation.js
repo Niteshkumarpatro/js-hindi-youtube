fcadf872c3d15ba2b06287792d86844cceedf967
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-03-28 12:48:17 +0530

    conversion of datatype

--- 04_conversionoperation.js
+++ 04_conversionoperation.js
@@ -1,11 +1,29 @@
 let score = "nitesh"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log(typeof (score));
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log( valueInNumber);
-console.log(typeof valueInNumber);
-console.log(typeof valueInNumber);
+ // console.log(typeof valueInNumber);
+ // console.log( valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "nitesh"
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+
+// 1=> true; 0=> false
+//"" => false
+//"nitesh" => true
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+
+console.log(stringnumber);
+console.log(typeof stringnumber);
