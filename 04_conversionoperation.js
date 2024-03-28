1acf21fd316744c0e7631f65efaff358df1e3610
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-03-28 13:50:09 +0530

    conversion of datatype

--- 04_conversionoperation.js
+++ 04_conversionoperation.js
@@ -25,5 +25,45 @@ let somenumber = 33
 
 let stringnumber = String(somenumber)
 
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+//***********************************************  operations **************************************************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "hi";
+let str2 = " nitesh";
+
+ let str3 =str1 + str2;
+
+ // console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1 +"2");
+// console.log("1"+"2");
+// console.log("1" + 2 + 2);
+// console.log(1 +"2"+ 2);
+
+// console.log( (3+4) *5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+
+num1 = num2= num3 = 2 + 2
+ let gamecounter = 100
+
+
+
+
