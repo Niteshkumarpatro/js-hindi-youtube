85344d149160863749cfda6e49c1b2e41dc83866
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-04-01 20:42:40 +0530

    conversion of datatypes

--- 04_conversionoperation.js
+++ 04_conversionoperation.js
@@ -1,21 +1,22 @@
 let score = "nitesh"
 
-// console.log(typeof score);
+ // console.log(typeof score);
 
-// console.log(typeof (score));
+ // console.log(typeof (score));
 
 let valueInNumber = Number(score)
- // console.log(typeof valueInNumber);
- // console.log(valueInNumber);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = "nitesh"
+let isLoggedIn ="nitesh"
 
 let booleanIsloggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsloggedIn);
+  // console.log(booleanIsloggedIn);
 
 // 1=> true; 0=> false
 //"" => false
@@ -32,7 +33,7 @@ let stringnumber = String(somenumber)
 
 let value = 3
 let negvalue = -value
-// console.log(negvalue);
+ // console.log(negvalue);
 
 // console.log(2+2);
 // console.log(2-2);
@@ -46,25 +47,26 @@ let str2 = " nitesh";
 
  let str3 =str1 + str2;
 
- // console.log(str3);
+  // console.log(str3);
 
-// console.log("1"+ 2);
-// console.log(1 +"2");
-// console.log("1"+"2");
-// console.log("1" + 2 + 2);
-// console.log(1 +"2"+ 2);
+ // console.log("1"+ 2);
+ // console.log(1 +"2");
+ // console.log("1"+"2");
+ // console.log("1" + 2 + 2);
+ // console.log(1 +"2"+ 2);
+ // console.log(1 +2+ "2");
 
-// console.log( (3+4) *5 % 3);
+ // console.log( (3+4) *5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+  // console.log(+true);
+  // console.log(+"");
 
 let num1,num2,num3
 
-num1 = num2= num3 = 2 + 2
+num1 = num2 = num3 = 2 + 2
  let gamecounter = 100
 gamecounter++;
-console.log(gamecounter);
+// console.log(gamecounter);
 
 
 
