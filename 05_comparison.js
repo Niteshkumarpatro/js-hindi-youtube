29c468d3224bf0b24bf6d19dfcb5592ea5e47162
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-04-01 21:15:47 +0530

    Comparison of datatypes

--- 05_comparison.js
+++ 05_comparison.js
@@ -15,6 +15,8 @@
  // COMPARISONS CONVERT NULL TO A NUMBER,TREATING IT AS 0.
  // THAT'S WHY (3) NULL>=O IS TRUE AND (1) NULL>0 IS FALSE
 
+
+
  // console.log(undefined == 0);
  // console.log(undefined > 0);
  // console.log(undefined < 0);
