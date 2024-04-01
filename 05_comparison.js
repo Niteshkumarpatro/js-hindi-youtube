a68f4d8d9578d90c037614c0aeb9ab8a39df5c5c
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-04-01 21:13:39 +0530

    Comparison of datatypes

--- 05_comparison.js
+++ 05_comparison.js
@@ -1,20 +1,23 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+ // console.log(2 > 1);
+ // console.log(2 >= 1);
+ // console.log(2 < 1);
+ // console.log(2 == 1);
+ // console.log(2 != 1);
 
 
-// console.log("2"> 1);
-// console.log("02" > 1);
+ // console.log("2"> 1);
+ // console.log("02" > 1);
 
- console.log(null > 0);
- console.log(null == 0);
- console.log(null >= 0);
+ // console.log(null > 0);
+ // console.log(null == 0);
+ // console.log(null >= 0);
+ // THE REASON IS THAT AN EQUALITY CHECK == AND COMPARISONS > < >= <= WORK DIFFERENTLY.
+ // COMPARISONS CONVERT NULL TO A NUMBER,TREATING IT AS 0.
+ // THAT'S WHY (3) NULL>=O IS TRUE AND (1) NULL>0 IS FALSE
 
- console.log(undefined == 0);
- console.log(undefined > 0);
- console.log(undefined < 0);
+ // console.log(undefined == 0);
+ // console.log(undefined > 0);
+ // console.log(undefined < 0);
 
 // ===
 
