dabc18b459e8b7b7d2e542698630b77339d5d602
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-04-02 20:39:52 +0530

    strings

---
+++ 07_string.js
@@ -0,0 +1,40 @@
+const name="nitesh"
+const repocount=50
+
+// console.log(name + repocount + " value");
+
+// console.log(`hello my name is ${name} and my repocount is ${repocount}`);
+
+
+ const gameName= new String("pubjbattle");
+
+// console.log(gameName[1]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(0));
+// console.log(gameName.indexOf("u"));
+
+// const newString = gameName.substring(0,5)
+// console.log(newString);
+
+
+const anotherstring = gameName.slice(-9,3);
+ console.log(anotherstring);
+
+ const newstring1 = "  nitesh "
+console.log(newstring1);
+console.log(newstring1.trim());
+
+const url = "https://nitesh%20kumar"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes("nitesh"))
+
+console.log(url.includes("hello"))
+
+console.log(gameName.split('-'));
+
+
