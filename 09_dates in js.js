bb68dc1bfea54be6b274aed6f87403001c02b627
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-04-02 20:49:36 +0530

    dates

---
+++ 09_dates in js.js
@@ -0,0 +1,24 @@
+// dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toUTCString());
+
+console.log(typeof myDate);
+
+  // let mycreatedDate = new Date(2024,09, 03);
+//  let mycreatedDate = new Date(2024,3, 2, 5, 3);
+//  let mycreatedDate = new Date("2024-03-03");
+// let mycreatedDate = new Date("04-02-2024");
+console.log(mycreatedDate.toLocaleString());
+
+
+
+
+
+
+
