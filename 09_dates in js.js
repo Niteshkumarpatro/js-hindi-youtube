a5b111869172702842a47c0b9644bc95db7d9058
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-04-04 11:26:56 +0530

    dates

--- 09_dates in js.js
+++ 09_dates in js.js
@@ -1,20 +1,57 @@
 // dates
 
-let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toJSON());
-// console.log(myDate.toLocaleDateString());
-// console.log(myDate.toLocaleTimeString());
-// console.log(myDate.toUTCString());
+ let myDate = new Date()
+ // console.log(myDate.toString());
+ // console.log(myDate.toDateString());
+ // console.log(myDate.toJSON());
+ // console.log(myDate.toLocaleDateString());
+ // console.log(myDate.toLocaleTimeString());
+ // console.log(myDate.toUTCString());
 
-console.log(typeof myDate);
+ // console.log(typeof myDate);
 
-  // let mycreatedDate = new Date(2024,09, 03);
-//  let mycreatedDate = new Date(2024,3, 2, 5, 3);
-//  let mycreatedDate = new Date("2024-03-03");
-// let mycreatedDate = new Date("04-02-2024");
-console.log(mycreatedDate.toLocaleString());
+  // let mycreatedDate = new Date(2024,09,03);
+  //  let mycreatedDate = new Date(2024,3, 2, 5, 3);
+  // let mycreatedDate = new Date("2024-12-03"); // yy/mm/dd
+     let mycreatedDate = new Date("01-14-2024"); // mm/dd/yy
+
+ // console.log(mycreatedDate.toLocaleString());
+
+ let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(mycreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getFullYear());
+// console.log(newDate.getTime());
+// console.log(newDate.getDate());
+// console.log(newDate.getHours());
+// console.log(newDate.getMilliseconds());
+// console.log(newDate.getSeconds());
+// console.log(newDate.getDay());
+
+
+// `${newDate.getDate()} and the time `
+
+newDate.toLocaleString('default',{
+   weekday:"long",
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 
