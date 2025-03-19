61d539c5e48148bee66522bc6f4f309af0c61eff
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2025-03-19 12:08:26 +0530

    CONVERSION OF DATATYPES

--- 04_conversionoperation.js
+++ 04_conversionoperation.js
@@ -1,72 +1,34 @@
-let score = "nitesh"
-
- // console.log(typeof score);
-
- // console.log(typeof (score));
+//let score="33abc"
+//let score=null
+let score=undefined
+//console.log(typeof score);
+ //console.log(typeof(score));
 
 let valueInNumber = Number(score)
-// console.log(valueInNumber);
-// console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 
-// "33" => 33
-// "33abc" => NaN
-// true => 1; false => 0
 
-let isLoggedIn ="nitesh"
+// "33" =>33
+// "33abc"=>nan
+//true => 1; false=>0
 
-let booleanIsloggedIn = Boolean(isLoggedIn)
-  // console.log(booleanIsloggedIn);
+let isLoggedIn=1
+let booleanisLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanisLoggedIn);
 
-// 1=> true; 0=> false
-//"" => false
-//"nitesh" => true
+//1=>true; 0=>false
+// ""=>false
+// "nitesh"=>true
 
-let somenumber = 33
+let somenumber=33
 
-let stringnumber = String(somenumber)
+let stringNumber=String(somenumber);
+console.log(stringNumber)
+console.log(typeof stringNumber)
 
-// console.log(stringnumber);
-// console.log(typeof stringnumber);
 
-//***********************************************  operations **************************************************
-
-let value = 3
-let negvalue = -value
- // console.log(negvalue);
-
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2/2);
-// console.log(2%3);
-
-let str1 = "hi";
-let str2 = " nitesh";
-
- let str3 =str1 + str2;
-
-  // console.log(str3);
-
- // console.log("1"+ 2);
- // console.log(1 +"2");
- // console.log("1"+"2");
- // console.log("1" + 2 + 2);
- // console.log(1 +"2"+ 2);
- // console.log(1 +2+ "2");
-
- // console.log( (3+4) *5 % 3);
-
-  // console.log(+true);
-  // console.log(+"");
-
-let num1,num2,num3
-
-num1 = num2 = num3 = 2 + 2
- let gamecounter = 100
-gamecounter++;
-// console.log(gamecounter);
 
 
 
