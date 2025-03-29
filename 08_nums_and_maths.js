c3b716f51b206c9ffde8ea486d83bbfaf19c22ff
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2025-03-29 14:02:42 +0530

    MATH AND NUM

--- 08_nums_and_maths.js
+++ 08_nums_and_maths.js
@@ -1,38 +1,35 @@
+const score=234
+//console.log(score);
 
- const score = 400
- // console.log(score)
+const balance= new Number(1000)
+//console.log(balance);
 
- const balance =new Number(100)
- // console.log(balance);
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(4))
 
- // console.log(balance.toString().length);
- //console.log(balance.toFixed(2));
+const otherNumber=23.3435
+//console.log(otherNumber.toPrecision(50));
 
- const othernumber = 423.234
+const hundreds=1000000
+//console.log(hundreds.toLocaleString('en-IN'));
 
- // console.log(othernumber.toPrecision(4));
+//*********************** maths **************************
 
- const hundreds = 1000000
- // console.log(hundreds.toLocaleString('en-in'));
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min(4,3,2,5,6));
+// console.log(Math.max(4,3,2,5,6));
+
+console.log(Math.random());// always show output b/w 0 and 1
+console.log(Math.floor(Math.random()*10)+1);
+
+const min =10;
+const max=20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
 
 
-//  ************************************  MATHS ****************************************************
-
- // console.log(Math);
- // console.log(Math.abs(-4));
- // console.log(Math.round(4.5));
- // console.log(Math.ceil(4.2));
- // console.log(Math.floor(4.5));
- // console.log(Math.min(4,5,6,7,8));
- // console.log(Math.max(4,5,6,7,8));
-
-
- console.log(Math.random());
- console.log((Math.random()*10) + 1);
- console.log(Math.floor(Math.random()*10) + 1);
-
- const min = 10
- const max = 20
-
- console.log(Math.floor(Math.random() * (max - min + 1)) + min)
 
