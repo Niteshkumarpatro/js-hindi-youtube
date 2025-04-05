01a254d527359e3691c143cc8c3e9f7371fbe775
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2025-04-05 16:03:52 +0530

    OBJECT

--- 13_objects2.js
+++ 13_objects2.js
@@ -27,7 +27,7 @@ const obj4={5:"a",6:"b"}
 //const obj3={obj1,obj2}
 //const obj3=Object.assign({},obj1,obj2,obj4)
 const obj3={...obj1,...obj2}
-console.log(obj3)
+//console.log(obj3)
 
 
 const users=[
@@ -44,11 +44,36 @@ const users=[
 ]
 
 
-users[1].email
-console.log(tinderUser);
+//users[1].email
+// console.log(tinderUser);
+//
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
 
-console.log(Object.keys(tinderUser))
-console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
+//console.log(tinderUser.hasOwnProperty('IsloggedIn'))
 
-console.log(tinderUser.hasOwnProperty('IsloggedIn'))
+const course={
+    coursename:"js in hindi",
+    price:"555",
+    courseInstructor:"chai aur code"
+
+}
+
+// course.courseInstructor
+
+const{courseInstructor:Instructor}=course
+//console.log(courseInstructor);
+console.log(Instructor)
+
+// {
+//     "name":"nitesh",
+//     "coursename":"js",
+//     "price":"free"
+// }
+
+// [
+//     {},
+//         {},
+//         {}
+//     ]
